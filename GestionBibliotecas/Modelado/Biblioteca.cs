a889b3fe5aab6a20b857bd3a1ac8b481b7cf06ad
BayronCampaz <35868120+BayronCampaz@users.noreply.github.com>
2018-01-27 15:04:11 -0500

    Add files via upload

---
+++ GestionBibliotecas/Modelado/Biblioteca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelado
+{
+    public class Biblioteca
+    {
+        private int num;
+        private int nombre;
+        private int direccion;
+    }
+}
